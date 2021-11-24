e4cda613d3aa11abc95c1b28f04c625540fccb1b
Fabius Grüner <fabiusgruener@gmx.de>
2021-11-24 07:53:03 +0100

    Projektdateien hinzufügen.

---
+++ Bücherladen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bücherladen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class bucher
+        {
+            public string author;
+
+        }
+    }
+}
